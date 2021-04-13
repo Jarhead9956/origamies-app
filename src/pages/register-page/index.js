25f043742ddc099cb3a49367ada2b05f3de269ee
hristo ilchev <jarhead9956@gmail.com>
2021-04-13 12:07:29 +0300

    Create login and register page funcionality

--- src/pages/register-page/index.js
+++ src/pages/register-page/index.js
@@ -10,50 +10,96 @@ class RegisterPage extends Component {
         super(props)
 
         this.state = {
-            email: '',
+            username: '',
             password: '',
-            repeatPassword: ''
+            repeatPassword: '',
+            registerError: false
         }
     }
 
-    onChange = (event, type) => {
+    handleChange = (event, type) => {
         const newState = {}
         newState[type] = event.target.value
 
         this.setState(newState)
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        const {
+            username,
+            password,
+            repeatPassword,
+            registerError
+        } = this.state
+
+        if((password.length < 1) || (repeatPassword.length < 1) || (password !== repeatPassword)){
+            this.setState({
+                registerError: true
+            })
+
+            return
+        }
+
+        fetch('http://localhost:9999/api/user/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(promise => {
+            const authToken = promise.headers.get('Autorization')
+            if(authToken){
+                document.cookie = `x-auth-token=${authToken}`
+            }
+            return promise.json()
+        }).then(data => {
+            if(data.username && document.cookie){
+                this.props.history.push('/')
+            }
+        }).catch(e => {
+            this.setState({
+                loginError: true
+            })
+        })
+    }
+
     render() {
         const {
-            email,
+            username,
             password,
-            repeatPassword
+            repeatPassword,
+            registerError
         } = this.state
 
         return(
             <PageLayout>
-                <div className={styles.container}>
+                <form className={styles.container} onSubmit={this.handleSubmit}>
                     <Title title='Register page' />
                     <Input 
-                        label='Email' 
-                        value={ email }
-                        onChange={(e) => this.onChange(e, 'email')} 
-                        id='email'>
+                        label='Username' 
+                        value={ username }
+                        onChange={(e) => this.handleChange(e, 'username')} 
+                        id='username'>
                     </Input>
                     <Input 
+                        type="password"
                         label='Password' 
                         value={ password } 
-                        onChange={(e) => this.onChange(e, 'password')} 
+                        onChange={(e) => this.handleChange(e, 'password')} 
                         id='password'>
                     </Input>
                     <Input 
+                        type="password"
                         label='Re-password' 
                         value={ repeatPassword }
-                        onChange={(e) => this.onChange(e, 'repeatPassword')} 
+                        onChange={(e) => this.handleChange(e, 'repeatPassword')} 
                         id='re-password'>
                     </Input>
-                    <SubmitButton title='Register' />
-                </div> 
+                    {registerError ? (<div>Password and Repeat passsword don't match!</div>) : null}
+                    <SubmitButton type='submit' title='Register' />
+                </form> 
             </PageLayout>
         )
     }
